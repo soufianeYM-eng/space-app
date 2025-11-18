73882fa33450c035f4c470dbabdddc3fe2fe068c
SufianYM <yousfisoufiane8@gmail.com>
2025-11-18 18:17:35 +0100

    feature(vite): update default PORT to 9000

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+const PORT = 9000
+
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   base: './',
+  server: {
+    port: PORT,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
